d7b54c1f41cbbe50d26c92354fd9fc669d9e3e3d
Brian Burton <brian.burton@adhocteam.us>
2022-03-08 17:22:43 -0500

    Adds support for line entities to script.

--- apps/bfd-model/bfd-model-rif/create-mappings.py
+++ apps/bfd-model/bfd-model-rif/create-mappings.py
@@ -11,29 +11,68 @@ classNameSuffix = "Z"
 def create_mapping(summary):
     mapping = dict()
     mapping["id"] = summary["headerEntity"]
-    mapping["entityClassName"] = f'{summary["packageName"]}.{summary["headerEntity"]}{classNameSuffix}'
+    mapping["entityClassName"] = f'{summary["packageName"]}.{mapping["id"]}{classNameSuffix}'
 
-    primaryKeyColumns = []
-    table = {"name": summary["headerTable"], "primaryKeyColumns": primaryKeyColumns}
+    primary_key_columns = []
+    table = {"name": summary["headerTable"], "primaryKeyColumns": primary_key_columns}
     mapping["table"] = table
 
-    fields = set()
     columns = list()
+    line_number_column_name = summary["lineEntityLineNumberField"]
     for rif_field in summary["rifLayout"]["fields"]:
-        field_name = rif_field["javaFieldName"]
-        if field_name not in fields:
-            columns.append(create_column(rif_field))
-            fields.add(field_name)
-        else:
-            sys.stderr.write(f'warning: {mapping["id"]} column {rif_field["rifColumnName"]} has a duplicate javaFieldName {field_name}\n')
+        if line_number_column_name == rif_field["rifColumnName"]:
+            break
+
+        columns.append(create_column(rif_field))
 
     table["columns"] = columns
 
     if summary["headerEntityGeneratedIdField"] is not None:
-        primaryKeyColumns.append(summary["headerEntityGeneratedIdField"])
+        primary_key_columns.append(summary["headerEntityGeneratedIdField"])
         columns.append(create_generated_id_column(summary["headerEntityGeneratedIdField"]))
     if summary["headerEntityIdField"] is not None:
-        primaryKeyColumns.append(summary["headerEntityIdField"])
+        primary_key_columns.append(summary["headerEntityIdField"])
+
+    return mapping
+
+
+def add_primary_key(summary, primary_key_columns, column_name):
+    field_name = find_field_name(summary, column_name)
+    primary_key_columns.append(field_name)
+
+
+def create_line_mapping(summary):
+    if not summary["hasLines"]:
+        return None
+
+    mapping = dict()
+    parent_name = summary["headerEntity"]
+    mapping["id"] = f'{parent_name}Line'
+    mapping["entityClassName"] = f'{summary["packageName"]}.{mapping["id"]}{classNameSuffix}'
+
+    primary_key_columns = []
+    table = {"name": mapping["id"], "primaryKeyColumns": primary_key_columns}
+    mapping["table"] = table
+
+    columns = list()
+    line_number_column_name = summary["lineEntityLineNumberField"]
+    line_fields_started = False
+    for rif_field in summary["rifLayout"]["fields"]:
+        if line_number_column_name == rif_field["rifColumnName"]:
+            line_fields_started = True
+
+        if not line_fields_started:
+            continue
+
+        columns.append(create_column(rif_field))
+
+    table["columns"] = columns
+
+    if summary["headerEntityGeneratedIdField"] is not None:
+        primary_key_columns.append(summary["headerEntityGeneratedIdField"])
+    if summary["headerEntityIdField"] is not None:
+        primary_key_columns.append(summary["headerEntityIdField"])
+    add_primary_key(summary, primary_key_columns, line_number_column_name)
 
     return mapping
 
@@ -77,6 +116,14 @@ def create_column(rif_field):
     return column
 
 
+def find_field_name(summary, column_name):
+    for rif_field in summary["rifLayout"]["fields"]:
+        if column_name == rif_field["rifColumnName"]:
+            return rif_field["javaFieldName"]
+    sys.stderr.write(f'error: {summary["headerEntity"]} has no {column_name} column\n')
+    raise
+
+
 summaryFilePath = Path("target/rif-mapping-summary.yaml")
 mappings = yaml.safe_load(summaryFilePath.read_text())
 
@@ -85,6 +132,9 @@ for mapping in mappings:
     # print(f'{mapping["packageName"]}.{mapping["headerEntity"]}')
     dsl = create_mapping(mapping)
     output_mappings.append(dsl)
+    dsl = create_line_mapping(mapping)
+    if dsl is not None:
+        output_mappings.append(dsl)
     # print(yaml.dump(dsl))
 
 result = {"mappings": output_mappings}
